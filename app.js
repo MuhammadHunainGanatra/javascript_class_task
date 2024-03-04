dc564b74a9408cc84994b1b8c8c0896d3f6cef18
Muhammad Hunain <153491211+MuhammadHunainGanatra@users.noreply.github.com>
2024-03-04 13:04:29 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+//CLASS PRACTICE//
+// var Weekday= prompt("Enter a number")
+// if(Weekday==1){
+//     console.log("Monday");
+// }
+// else if(Weekday==2){
+//     console.log("Tuesday");
+// }
+// else if(Weekday==3){
+//     console.log("Wednesday");
+// }
+// else if(Weekday==4){
+//     console.log("Thursday");
+// }
+// else if(Weekday ==5){
+//     console.log("Friday");
+// }
+// else if(Weekday==6){
+//     console.log("Saturday");
+// }
+// else if(Weekday==7){
+//     console.log("Sunday Holiday ✌")
+// }
+// else{
+//     console.log("Enter a number 1 to 7");
+// }
+
+//Task1//
+// number = 6;
+// if(number % 2 ==0 && number % 3 ==0){
+// console.log("condition True");
+// }
+// else{
+//     console.log("condition False")
+// } 
+
+//Task2//
+// var practical= prompt("enter your Practical Marks");
+// var Thoery= prompt("enter your Thoery Marks");
+// if(practical >=15 && Thoery>=60 ){
+//     console.log("HAPPY YOU ARE PASSED")
+// }
+// else{
+//     console.log("You Are Fail")
+// }
+
+// //Task3//
+// var age = prompt("enter your Practical Marks");
+// if(age >17){
+//     console.log("VALID ID CARD")
+// }
+// else{
+//     console.log("NOT ELIGIBLE FOR VOTE")
+// }
+
+Task4
